2a5d899a097f1b5827d119a3bdaf56521f7034f4
Spencer Mattison <spenceramattison@gmail.com>
2025-10-03 16:10:24 +0900

    Create validations for Property
    Added validations to Property Model for unique_id, property_name and room_number. Added enum for type_of_property.

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,11 @@
 class Property < ApplicationRecord
+    enum type_of_property: {
+        mansion: 0,
+        apartment: 1,
+        house: 2
+    }
+
+    validates :unique_id, presence: true
+    validates :property_name, presence: true
+    validates :room_number, presence: true, unless: :house?
 end
